daa80f391071d709f68f2ca04044d4fe7d8a4f11
TOSHIUBA\Brenda <brendamorpe@gmail.com>
2018-10-30 13:27:10 -0600

    Updates

--- precache-manifest.fcca8e39204370e3338ff4ebf5da9119.js
+++ precache-manifest.fcca8e39204370e3338ff4ebf5da9119.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/HackathonHERE/static/css/main.a40a18c4.chunk.css"
   },
   {
-    "revision": "94998475f6aea65f558494802416c1cf",
-    "url": "/HackathonHERE/static/media/Roboto-Thin.94998475.ttf"
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/HackathonHERE/static/media/Roboto-Bold.e31fcf18.ttf"
   },
   {
     "revision": "4769a7df603fb0c27785",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "/HackathonHERE/static/media/arrow_right.8ef6a08c.svg"
   },
   {
-    "revision": "dfe56a876d0282555d1e2458e278060f",
-    "url": "/HackathonHERE/static/media/Roboto-Thin.dfe56a87.eot"
-  },
-  {
     "revision": "a990f611f2305dc12965f186c2ef2690",
     "url": "/HackathonHERE/static/media/Roboto-Light.a990f611.eot"
   },
   {
     "revision": "30799efa5bf74129468ad4e257551dc3",
     "url": "/HackathonHERE/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/HackathonHERE/static/media/Roboto-Thin.dfe56a87.eot"
   },
   {
     "revision": "df99a662573428de7e66",
@@ -84,16 +84,16 @@ self.__precacheManifest = [
     "url": "/HackathonHERE/static/media/Roboto-Regular.2751ee43.woff2"
   },
   {
-    "revision": "69f8a0617ac472f78e45841323a3df9e",
-    "url": "/HackathonHERE/static/media/Roboto-Light.69f8a061.woff2"
-  },
-  {
     "revision": "574fd0b50367f886d359e8264938fc37",
     "url": "/HackathonHERE/static/media/Roboto-Medium.574fd0b5.woff2"
   },
   {
     "revision": "39b2c3031be6b4ea96e2e3e95d307814",
     "url": "/HackathonHERE/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/HackathonHERE/static/media/Roboto-Light.69f8a061.woff2"
   },
   {
     "revision": "7500519de3d82e33d1587f8042e2afcb",
@@ -116,20 +116,20 @@ self.__precacheManifest = [
     "url": "/HackathonHERE/static/media/Roboto-Bold.dc81817d.woff"
   },
   {
-    "revision": "46e48ce0628835f68a7369d0254e4283",
-    "url": "/HackathonHERE/static/media/Roboto-Light.46e48ce0.ttf"
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/HackathonHERE/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/HackathonHERE/static/media/Roboto-Medium.894a2ede.ttf"
   },
   {
     "revision": "df7b648ce5356ea1ebce435b3459fd60",
     "url": "/HackathonHERE/static/media/Roboto-Regular.df7b648c.ttf"
   },
   {
-    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
-    "url": "/HackathonHERE/static/media/Roboto-Bold.e31fcf18.ttf"
-  },
-  {
-    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
-    "url": "/HackathonHERE/static/media/Roboto-Medium.894a2ede.ttf"
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/HackathonHERE/static/media/Roboto-Light.46e48ce0.ttf"
   },
   {
     "revision": "4769a7df603fb0c27785",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HackathonHERE/precache-manifest.3bf6b20505cfa882d5f4aca630b49420.js"
+  "/HackathonHERE/precache-manifest.fcca8e39204370e3338ff4ebf5da9119.js"
 );
 
 workbox.clientsClaim();
